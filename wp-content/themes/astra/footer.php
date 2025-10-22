743478ce5a6b91dfcd3c36730501fce13e28cf71
Miguel Faggioni <miguel@faggioni.dev>
2025-10-22 10:11:59 -0400

    theme

---
+++ wp-content/themes/astra/footer.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Astra
+ * @since 1.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+?>
+<?php astra_content_bottom(); ?>
+	</div> <!-- ast-container -->
+	</div><!-- #content -->
+<?php 
+	astra_content_after();
+		
+	astra_footer_before();
+		
+	astra_footer();
+		
+	astra_footer_after(); 
+?>
+	</div><!-- #page -->
+<?php 
+	astra_body_bottom();    
+	wp_footer(); 
+?>
+    <?php echo do_shortcode('[hq_jivo_chat_widget]'); ?>
+    <script>
+    /*
+  window.intercomSettings = {
+    api_base: "https://api-iam.intercom.io",
+    app_id: "ofc97xtr"
+	  };
+	</script>
+	<script>
+	// We pre-filled your app ID in the widget URL: 'https://widget.intercom.io/widget/ofc97xtr'
+	(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/ofc97xtr';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(document.readyState==='complete'){l();}else if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
+    */</script>
+	</body>
+</html>
